1b0c30a14146f982463cef351ddee87a854e0c50
Marfutenko, Petr <petr.marfutenko@x5.ru>
2025-01-31 06:18:58 +0300

    fix request

--- main_agent.py
+++ main_agent.py
@@ -14,7 +14,7 @@ def api_request(body):
 
         return requests.post(api_url, headers, json = body)
 
-def get_check_is_valid_messages (answer: str):
+def get_check_is_valid_messages (question):
      return [
        
             {
@@ -24,9 +24,28 @@ def get_check_is_valid_messages (answer: str):
             {
                 "role": "user",
             
-                "content": answer
+                "content": question
             }
     ]
+
+def get_fetch_question_messages (question, context):
+     return [
+       
+            {
+                "role": "system",
+                "content": "Ты помошник отвечающий на тесты. Тебе на вход подается строка - вопрос следующего вида - вопрос потом символ \n потом пронумерованные варианты ответов, разделенные символом \n. Как вопрос, так и варианты ответов могут содержать символ \n внутри. Вопросы, которые тебе задаются, почти всегда содержат варианты ответов, пронумерованные цифрами от 1 до 10. Каждый вариант ответа соответствует определённому утверждению или факту. Ты должен определить правильный вариант ответа и вернуть его номер (это очень важно - только номер, без содержания ответа) в поле answer JSON-ответа. Если вопрос не предполагает выбор из вариантов, или вариантов ответа нет:, то поле answer должно содержать null."
+            },
+              {
+                "role": "assistant",
+                "content": context
+            },
+            {
+                "role": "user",
+            
+                "content": question
+            }
+        ]
+
 class AgentRequest:
     
     def llm(self, messages):
@@ -37,6 +56,10 @@ class AgentRequest:
             
 
 class Main_actor:
-    def check_is_answer_valid(self, answer):
+    def check_is_question_valid(self, question):
          Fetch = AgentRequest()
-         Fetch.llm(answer)
+         return Fetch.llm(get_check_is_valid_messages(question))
+
+    def get_answer(self, question, context):
+         Fetch = AgentRequest()
+         return Fetch.llm(get_fetch_question_messages(question, context))
